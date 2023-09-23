7b6e990fb03e23d2b1eae4ff54c362d0dbe268bd
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-23 14:50:50 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "middle.h"
+using namespace std;
+long long itc_multi_num(long long number){
+    int b = 1;
+    if (number == 0){
+        return 0;
+    }
+    while(number > 0){
+        b = b * (number%10);
+        number = number / 10;
+    }
+    return b;
+
+}

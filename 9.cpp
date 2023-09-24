7e4411d2a312af9f8a54756a4f787159da3b4265
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-24 16:56:14 +0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "middle.h"
+using namespace std;
+bool itc_mirror_num(long long number){
+    int b = 0, num = number;
+    while (number){
+        b = b*10 + number%10;
+        number = number / 10;
+    }
+    if (b == num){
+        return true;
+    }
+    return false;
+}

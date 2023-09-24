a82abef330dfbaa0bad3a0928f9a8177ded5bc1d
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-24 16:57:15 +0300

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 bool itc_mirror_num(long long number){
     int b = 0, num = number;
-    while (number){
+    while (number > 0){
         b = b*10 + number%10;
         number = number / 10;
     }

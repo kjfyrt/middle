4200c6bdc9c9326d75b2e7619876a96bfe6bcd25
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-24 17:58:29 +0300

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -5,7 +5,7 @@ int itc_rev_bin_num(long long number) {
     long long n = 0, m = 1;
     while (number > 0) {
         n = n + (number%10)*m;
-        m = m*8;
+        m = m*2;
         number = number / 10;
     }
 

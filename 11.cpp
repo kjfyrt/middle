e9d6a8a1ba367b34e323f27ce4b40aef58da0432
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-24 17:23:07 +0300

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "middle.h"
+using namespace std;
+int itc_second_max_num(long long number){
+    int max = 0; int max2 = 0;
+    if (number / 10 == 0){
+        return -1;
+    }
+    while (number > 0){
+        if (number%10 >= max){
+            max2 = max;
+            max = number % 10;
+        }
+        if (number%10 > max2 and number%10 < max){
+            max2 = number % 10;
+        }
+        number = number / 10;
+    }
+    return max2;
+}

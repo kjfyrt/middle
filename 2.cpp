29fc5d64c9a28c1709cc4aa740aaa5655858fdd6
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-25 21:12:32 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 int itc_len_num(long long number){
     int b = 0;
+    if (number == 0){
+        return -1;
+    }
     while(number > 0){
         b++;
         number = number / 10;

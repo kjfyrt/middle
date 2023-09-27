5fa96052c7e9450923b1db3d1211a4498b103933
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-27 10:45:57 +0300

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int itc_second_max_num(long long number){
     int max = 0; int max2 = 0;
-    if (number / 10 == 0){
+    if(number<0){
+        number = -number;
+    }
+    if (number <= 9){
         return -1;
     }
     while (number > 0){
@@ -11,7 +14,7 @@ int itc_second_max_num(long long number){
             max2 = max;
             max = number % 10;
         }
-        if (number%10 > max2 and number%10 < max){
+        if (number%10 >= max2 and number%10 < max){
             max2 = number % 10;
         }
         number = number / 10;

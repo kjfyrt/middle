d7bca049b922ecf9185405be09f54534c39eedf6
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-27 10:55:43 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 int itc_sum_num(long long number){
     int b = 0;
+    if (number<0){
+        number = -number;
+    }
     while(number > 0){
         b = b + number%10;
         number = number / 10;
@@ -10,3 +13,4 @@ int itc_sum_num(long long number){
     return b;
 
 }
+
